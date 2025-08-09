88fa95700abbe1e9acdb733249b0db6c0ed67940
DreamerAkhilesh <imakhileshpratap@gmail.com>
2025-08-09 21:40:28 +0530

    Added comments

--- src/Config/database.js
+++ src/Config/database.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 
+// Do this mongoose.connect() inside an async function because it returns a Promise
+// and we want to use await to ensure the database connection is established before proceeding.
 const connectDB = async () => {
   try {
     await mongoose.connect(process.env.MONGO_URI);
@@ -10,3 +12,7 @@ const connectDB = async () => {
 };
 
 module.exports = connectDB;
+
+// Here we are only writing the function and we are connecting the db in the app.js file 
+// We could have also done the calling in this file only, and just need to do require() in the app.js to call the whole page there.(But not the best way to do.)
+// We need to make sure that the app.listen should only be done after the DB is established.
